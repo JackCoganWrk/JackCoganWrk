b56f0fbd1450f35e47d636eac97e313899dd3ed2
JackCoganWrk <19Cogan.J@isaacnewtonsixthform.co.uk>
2025-02-25 11:10:31 +0000

    Add files via upload

---
+++ Graph.java
@@ -0,0 +1,29 @@
+
+public class Graph {
+
+    String[] graph = new String[48];
+    int[] edgeWeight = new int[45];
+
+
+    void graphMakerDijkstras() {
+
+        graph[0] = "A";
+        graph[1] = "B";
+        graph[2] = "C";
+        graph[3] = "D";
+        graph[4] = "E";
+        graph[5] = "F";
+        graph[6] = "G";
+        graph[7] = "H";
+        edgeWeight[0] = 0;
+        edgeWeight[1] = 1;
+        edgeWeight[2] = 2;
+        edgeWeight[3] = 3;
+        edgeWeight[4] = 4;
+        edgeWeight[5] = 5;
+        edgeWeight[6] = 6;
+        edgeWeight[7] = 7;
+        edgeWeight[8] = 8;
+        edgeWeight[9] = 9;
+    }
+}
--- Main.java
+++ Main.java
@@ -1,59 +1,6 @@
-package io.github.some_example_name;
-
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.*;
-import com.badlogic.gdx.utils.ScreenUtils;
-
-
-public class Main extends ApplicationAdapter {
-
-    private SpriteBatch batch;
-    private Texture image;
-    ShapeRenderer sr;
-    private int screenHeight;
-    private int screenWidth;
-    private BoardSquare square;
-
-    @Override
-    public void create() {
-        screenHeight = Gdx.graphics.getHeight();
-        screenWidth = Gdx.graphics.getWidth();
-        sr = new ShapeRenderer();
-    }
-
-    @Override
-    public void render() {
-
-        //Getter methods for square dimensions
-        float xSquare = square.getxSquare();
-        float ySquare = square.getySquare();
-        float widthSquare = square.getWidthSquare();
-        float heightSquare = square.getHeightSquare();
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-
-        //Draws Array
-
-        sr.begin();
-        sr.rect(xSquare,ySquare,widthSquare,heightSquare);
-        sr.end();
-
-        batch.begin();
-        batch.draw(MakeBoard,xSquare,ySquare);
-        batch.end();
-    }
-
-    @Override
-    public void dispose() {
-        batch.dispose();
-        image.dispose();
+public class Main {
+    public static void main(String[] args) {
+        Graph graphMakerDijkstras = new Graph();
+        graphMakerDijkstras.graphMakerDijkstras();
     }
 }
